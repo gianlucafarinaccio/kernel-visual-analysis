52d4eee6bc562ce6017421e06b0d95c1d94d632c
isRoutine <gianluca.farinaccio@gmail.com>
2022-09-04 18:31:22 +0200

    graph from .dot local file

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return render_template('index.html')
 
+# This route show an example of graph imported by a .DOT file
+# The string which contain the file data is stored inside the
+# template's variable 'graph'
 @app.route('/dot')
 def dot():
-    return render_template('dot_example.html')
+    file = open('static/dots/network_10.dot', 'r')
+    graph = file.read()
+    return render_template('dot_example.html', graph=" '{}'".format(graph))
 
 if __name__ == '__main__':
     app.run()
--- dot_creator.py
+++ dot_creator.py
@@ -1,13 +1,15 @@
 def create_dot(nodes):
     default_path = 'static/dots/'
     file = open(default_path + "network_{}.dot".format(nodes), 'w')
-    file.write("digraph { \n")
+    file.write("digraph { ")
 
     for x in range(1, nodes + 1):
         for e in range(1, nodes + 1):
             if x == e:
                 continue
-            row = " {} -> {} ; \n".format(x, e)
+            row = " {} -> {} ;".format(x, e)
             file.write(row)
     file.write("}")
     file.close()
+
+create_dot(3)
