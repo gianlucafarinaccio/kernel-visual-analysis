0ba679ed163ec3e8761d33223c7d4b738ec84c83
isRoutine <gianluca.farinaccio@gmail.com>
2022-09-19 12:29:10 +0200

    storing data from vis to flask

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html')
+
 
 # This route show an example of graph imported by a .DOT file
 # The string which contain the file data is stored inside the
@@ -13,7 +17,7 @@ def hello_world():  # put application's code here
 def dot():
     file = open('static/dots/network_100.dot', 'r')
     graph = file.read()
-    return render_template('dot_example.html', graph= '"{}"'.format(graph))
+    return render_template('dot_example.html', graph='"{}"'.format(graph))
 
 
 # This route show an example of kmallocx function imported by a .DOT file
@@ -32,12 +36,17 @@ def physics():
     graph = file.read()
     file = open('static/positionsData.json', 'r')
     positions = file.read()
-    return render_template('physics.html', graph=' "{}"'.format(graph),positions = positions )
+    return render_template('physics.html', graph=' "{}"'.format(graph), positions=positions)
 
-@app.route('/getPositions/<string:data>', methods=['POST'])
-def getData(data):
-    file = open('static/positionsData.json', 'w')
-    file.write(data)
+
+@app.route('/getPositions', methods=["POST"])
+def getData():
+    jsonData = request.get_json()
+    print(jsonData)
+    with open('static/positionsData.json', 'w') as json_file:
+        json.dump(jsonData, json_file)
+    return 'Flask: data received!'
+
 
 if __name__ == '__main__':
     app.run()
