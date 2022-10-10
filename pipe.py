1d149cbb8d43c80819c69432face237ff7929051
isRoutine <gianluca.farinaccio@gmail.com>
2022-10-10 15:40:58 +0200

    clean

--- pipe.py
+++ pipe.py
@@ -1,6 +1,6 @@
 import subprocess
 
-
+###
 def nav():
 	#with open('static/dots/temp.txt','w') as output_dir:
 	completed_process = subprocess.run(["./nav", "-f","conf.json"], capture_output=True, text=True)  #run ls command and redirect stdout in a file
